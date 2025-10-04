f196b7658d6f5c09e37d4af8105655167154e17c
rahul variya <variyarahul195@gmail.com>
2025-10-04 11:16:18 +0530

    Updated

--- JDBC_Table_create.java
+++ JDBC_Table_create.java
@@ -4,12 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-
-
-public class JDBC_DB_create {
-	public void createDatabase() {	
+public class JDBC_Table_create {
+	public void createTable(){
 		try {
-			String url ="jdbc:mysql://localhost:3306/";
+			String url ="jdbc:mysql://localhost:3306/mysql";
 			String userName="root";
 			String password="R@hulMySQL@1410";
 			
@@ -18,26 +16,23 @@ public class JDBC_DB_create {
 			Statement stm=conn.createStatement();
 			
 			//create statement
-			String query = "create database Hello_World;";
-			
+			String query = "create table Class_v (sid int(3),sname varchar(20),semail varchar(200));";
+			String value = "insert into Class_v(sid,sname,semail) values(1,'Rahul','variyarahul123@gmail.com');";
 			//execute query
 			stm.execute(query);
-			System.out.println("DB Created succesfully");
+			System.out.println("Table Created succesfully");
+			
+			stm.execute(value);
+			System.out.println("Data Inserted succesfully");
 			
 			//close the connection
 			conn.close();
-			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args){
-		
-		
-							
+			
+	}
+
 }
-}
-
-
-
--- Main.java
+++ Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JDBC_DB_create st = new JDBC_DB_create();
+		JDBC_Table_create tb = new JDBC_Table_create();
 		//st.createDatabase();
-		st.createTable();
+		tb.createTable();
 
 	}
 
